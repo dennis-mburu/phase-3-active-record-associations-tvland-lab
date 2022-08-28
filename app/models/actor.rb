4c312a33ad91c5aaed03770ac74783de36feed61
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2022-08-28 10:29:16 +0300

    Done correctly, but tests still not passing

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,27 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+
+    def characters
+        self.characters
+    end
+
+    def shows
+        self.shows
+    end
+
+    def full_name
+        first = self.first_name
+        last = self.last_name
+        full_name = first + " " + last
+    end
+
+    def list_roles
+        all_characters = self.characters
+        all_characters.map do |char|
+            "#{char.name} - #{char.show.name}"
+        end
+    end
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,19 @@
 class Show < ActiveRecord::Base
-  
+    belongs_to :network
+    has_many :characters
+    has_many :actors, through: :characters
+
+    def characters
+        self.characters
+    end
+
+    def network
+        self.network
+    end
+
+    def actors_list
+        self.actors.map do |actor|
+            actor.full_name
+        end
+    end
 end
